e3933801801839e0a604f8deba9dc8aca99771b2
Duc Tran <duc.trantrung@outlook.com>
2016-06-16 03:05:46 +0700

    restyle history view

--- BSE365/BSE365.Web/app/main/transaction/transactionInfoHistoryController.js
+++ BSE365/BSE365.Web/app/main/transaction/transactionInfoHistoryController.js
@@ -16,6 +16,26 @@ mainApp.controller('transactionInfoHistoryController',
                     console.log(response);
                     $scope.data = response.data;
                     tableState.pagination.numberOfPages = Math.ceil(response.totalItems / tableState.pagination.number);
+                    loadTransaction($scope.data);
+                });
+        }
+
+        function loadTransaction(transactions) {
+            _.each(transactions,
+                function (item) {
+                    item.isGiving = item.currentAccount == item.giverId;
+                    item.isReceiving = item.currentAccount == item.receiverId;
+
+                    item.isAllowConfirmGave = item.isGiving && item.state == TransactionState.Begin;
+                    item.isAllowConfirmReceived = item.isReceiving && item.state == TransactionState.Transfered;
+
+                    item.isAllowAbandonTransaction = false;
+
+                    item.isAllowAttachment = item.state != TransactionState.Abandoned;
+                    item.isAllowUploadAttachment = item.isGiving &&
+                    (item.state == TransactionState.Begin || item.state == TransactionState.Transfered);
+
+                    item.isAbandoned = item.state == TransactionState.Abandoned;
                 });
         }
 
